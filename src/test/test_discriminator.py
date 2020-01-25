2d559749e5a3b9a5acfd398b9f2af4ae68bb8e71
raahii <piyo56.net@gmail.com>
2020-01-25 13:30:31 +0900

    add test for gradient discriminator #5

--- src/test/test_discriminator.py
+++ src/test/test_discriminator.py
@@ -2,7 +2,9 @@ import unittest
 
 import torch
 
-from discriminator import ImageDiscriminator, VideoDiscriminator
+import context
+from discriminator import (GradientDiscriminator, ImageDiscriminator,
+                           VideoDiscriminator)
 
 IMAGE_SIZE = 64
 VIDEO_LENGTH = 16
@@ -49,6 +51,26 @@ class TestModelForward(unittest.TestCase):
         xc = torch.empty(xc_shape, device=vdis.device).normal_()
 
         output = vdis(xg, xc)
+
+        expected = (BATCHSIZE, 4, 4, 4)
+        self.assertEqual(expected, output.shape)
+
+    def test_gradient_discriminator(self):
+        inputs = {
+            "ch1": GEOMTRIC_INFO_CH,
+            "ch2": COLOR_CH,
+            "use_noise": True,
+            "noise_sigma": 0.2,
+        }
+        gdis = GradientDiscriminator(**inputs)
+
+        xg_shape = (BATCHSIZE, GEOMTRIC_INFO_CH, VIDEO_LENGTH, IMAGE_SIZE, IMAGE_SIZE)
+        xg = torch.empty(xg_shape, device=gdis.device).normal_()
+
+        xc_shape = (BATCHSIZE, COLOR_CH, VIDEO_LENGTH, IMAGE_SIZE, IMAGE_SIZE)
+        xc = torch.empty(xc_shape, device=gdis.device).normal_()
+
+        output = gdis(xg, xc)
 
         expected = (BATCHSIZE, 4, 4, 4)
         self.assertEqual(expected, output.shape)
